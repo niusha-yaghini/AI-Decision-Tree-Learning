c2fad68031ecae17b7eb65e5b4b227a864655e06
Niusha_ygn <niusha.yaghini@gmail.com>
2023-04-12 14:04:02 +0430

    ex

--- Program.py
+++ Program.py
@@ -6,22 +6,7 @@ import Attribute
 #reading data
 data = pd.read_csv('test.csv')
 
-rows = list(data)
-print(rows[1])
-# pd.set_option('display.max_rows', None)
-# for r in data:
-#     print(r)
-# print(data.index)
-
-# print(data.index)
-# for i in data.index:
-#     print()
-# print(data[0].tolist)
-# print(data[1].tolist)
-# for row in data:
-#     # print(''.join(row))
-#     print(row)
-
+print(list(data.loc[[15]]))
 
 numOfAttributes = len(data.columns) - 1
 numOfExamples = len(data.index)
@@ -33,10 +18,9 @@ print(numOfExamples)
 print(AttributesNames)
 
 #dividing data into educational and experimental with 50 and 50
-trainExamples = originalList[:]
-testExamples = originalList[:]
+# trainExamples = originalList[:]
+# testExamples = originalList[:]
 
 #list of class nodes of attributes
-
 var = Attribute.AttributesAndValues(data, AttributesNames)
 # print(var)
